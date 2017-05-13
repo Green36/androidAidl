0212843328916243f43afc1f2e1d7a7c7393229e
pj <p@j>
2017-05-13 16:44:02 +0900

    update

--- app/src/main/java/com/sample/k/androidsampleaidl/CalculatorService.java
+++ app/src/main/java/com/sample/k/androidsampleaidl/CalculatorService.java
@@ -3,6 +3,7 @@ package com.sample.k.androidsampleaidl;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteCallbackList;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -15,27 +16,64 @@ public class CalculatorService extends Service {
     }
 
     private ICalculatorService.Stub mStub = new ICalculatorService.Stub() {
+        private RemoteCallbackList<ICalculatorCallback> mCallbacks = new RemoteCallbackList<ICalculatorCallback>();
 
         @Override
         public void registerCallback(ICalculatorCallback callback) {
+            Log.d(TAG, "registerCallback");
+            mCallbacks.register(callback);
         }
 
         @Override
         public void unregisterCallback(ICalculatorCallback callback) {
+            Log.d(TAG, "unregisterCallback");
+            mCallbacks.unregister(callback);
         }
 
         @Override
         public int add(int lhs, int rhs) throws RemoteException {
+            Log.d(TAG, "add (" + lhs + " + " + rhs + " = " + (lhs + rhs) + ")");
             return lhs + rhs;
         }
 
         @Override
         public void sum(List values) {
+            // 上の SuppressWarnings は、AIDL には generics は使用できないため
+            Log.d(TAG, "sum");
+            int total = 0;
+            for (Object obj : values) {
+                int value = (Integer) obj;
+                total += value;
+            }
+            Log.d(TAG, "total=" + total);
+            int n = mCallbacks.beginBroadcast();
+            for (int i = 0; i < n; i++) {
+                try {
+                    mCallbacks.getBroadcastItem(i).resultSum(total);
+                    Log.d(TAG, "called callback");
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                    Log.d(TAG, e.getMessage(), e);
+                }
+            }
+            mCallbacks.finishBroadcast();
         }
 
         @Override
         public void rotate(int[] array, int num) {
-
+            Log.d(TAG, "rotate");
+            int[] copy = new int[array.length];
+            System.arraycopy(array, 0, copy, 0, array.length);
+            int n = num % array.length;
+            if (n < 0) {
+                n = array.length + n;
+            }
+            for (int i = 0; i < array.length; i++) {
+                int to = (i + n) % array.length;
+                array[to] = copy[i];
+                Log.d(TAG, "array[" + to + "] = copy[" + i + "] (" + array[to]
+                        + " " + copy[i] + ")");
+            }
         }
 
         @Override
